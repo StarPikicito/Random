8e9c6d2ed0d68c90027ae45105a2f659077850ea
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-24 22:05:58 +0300

    Update clogevade.lua

--- .x.vr.e.hi/misc/clogevade.lua
+++ .x.vr.e.hi/misc/clogevade.lua
@@ -1,5 +1,5 @@
 [Evade] [10/24/2022]
 [+] Teleport to objective
 [~] Tweaked Annoy Downed
-[~] Fixed stopping walkspeed boost on dying
+[~] Fixed stopping walkspeed boost on dying, max boost in 2 now instead of 0.5
 [~] Tweaked Autofarm Wins
