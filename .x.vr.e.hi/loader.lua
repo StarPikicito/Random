d71d151ef74e1d8fe555db7364538f556a37db43
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-26 20:16:11 +0300

    Update loader.lua

--- .x.vr.e.hi/loader.lua
+++ .x.vr.e.hi/loader.lua
@@ -2,6 +2,6 @@ if game.PlaceId == 2597632885 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/snoday.lua"))()
   elseif game.PlaceId == 6839171747 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/doors.lua"))()
- elseif game.GameId == 3647333358 then
+ elseif game.GameId == 3647333358 or game.PlaceId == 9872472334 then
       loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/evade.lua"))()
 end
