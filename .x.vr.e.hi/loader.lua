d220526e35d0234bc487f51110865786d8475966
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2023-01-10 14:31:59 +0300

    yes

--- .x.vr.e.hi/loader.lua
+++ .x.vr.e.hi/loader.lua
@@ -1,3 +1,15 @@
+if isfile and writefile and typeof(isfile) == 'function' and typeof(writefile) == 'function' then
+	if not isfile('PromptedDiscordDH.txt') then
+		writefile('PromptedDiscordDH.txt', game:GetService('HttpService'):JSONEncode('hi'))
+		local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+		Module.Prompt({
+			invite = "https://discord.gg/fNeggqVMZs",
+			name = "CF-Community",
+		})
+	end
+end
+
+
 if game.PlaceId == 2597632885 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/snoday.lua"))()
   elseif game.PlaceId == 6839171747 then
