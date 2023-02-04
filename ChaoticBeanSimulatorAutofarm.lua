3c0967dfc6584d18388556c688a9cdcb98551f54
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-04 22:53:23 +0300

    Update ChaoticBeanSimulatorAutofarm.lua

--- ChaoticBeanSimulatorAutofarm.lua
+++ ChaoticBeanSimulatorAutofarm.lua
@@ -1,10 +1,16 @@
 -- we love SkyTheFloof
-repeat task.wait() until game:IsLoaded()
+-- szze#6220
+repeat
+	task.wait()
+until game:IsLoaded()
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 local httpservice = game:GetService('HttpService')
 local Players = game:GetService('Players')
-local blacklistedids = {9779414,46045403}
+local blacklistedids = {
+	9779414,
+	46045403
+}
 
 function shop()
 	local gameId
@@ -23,9 +29,9 @@ function shop()
 	end
 	task.spawn(function()
 		while task.wait(0.5) do
-				if #servers > 0 then
-					game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, servers[math.random(1, #servers)], Players.LocalPlayer)
-				end
+			if #servers > 0 then
+				game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, servers[math.random(1, #servers)], Players.LocalPlayer)
+			end
 		end
 	end)
 	game:GetService("TeleportService").TeleportInitFailed:Connect(function()
@@ -35,41 +41,41 @@ end
 
 queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/random/main/ChaoticBeanSimulatorAutofarm.lua'))()")
 
-for i,v in next, game:GetService('Players'):GetPlayers() do
-    for _, id in next, blacklistedids do -- SkyTheFloof and Kocmoc (snitch)
-	if v.UserId == id then
-	   Players.LocalPlayer:Kick('Serverhopping, blacklisted id (dev) joined')
-          shop()
-        end
-    end
+for i, v in next, game:GetService('Players'):GetPlayers() do
+	for _, id in next, blacklistedids do
+		if v.UserId == id then
+			Players.LocalPlayer:Kick('Serverhopping, blacklisted id [aka dev] joined')
+			shop()
+		end
+	end
 end
 
 game:GetService('Players').PlayerAdded:Connect(function(v)
-    for _, id in next, blacklistedids do -- SkyTheFloof and Kocmoc (snitch)
-	if v.UserId == id then
-	   Players.LocalPlayer:Kick('Serverhopping, blacklisted id (dev) joined')
-          shop()
-        end
-    end
+	for _, id in next, blacklistedids do
+		if v.UserId == id then
+			Players.LocalPlayer:Kick('Serverhopping, blacklisted id [aka dev] joined')
+			shop()
+		end
+	end
 end)
 
 task.spawn(
     function()
-        while task.wait() do
-            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("OnLandFromJump"):FireServer(
+	while task.wait() do
+		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("OnLandFromJump"):FireServer(
                 "\226\128\139"
             )
-        end
-    end
+	end
+end
 )
 while task.wait() do
-    game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("Ascend"):InvokeServer()
-    game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("Rebirth"):InvokeServer()
-    for i, v in next, workspace:GetDescendants() do
-        if v.Name == "GumdropHitbox" and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Bean") then
-            game:GetService("Players").LocalPlayer.Character.Bean.CFrame = CFrame.new(v.Position)
-            task.wait(0.1)
-            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("InteractObject"):FireServer(v)
-        end
-    end
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("Ascend"):InvokeServer()
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("Rebirth"):InvokeServer()
+	for i, v in next, workspace:GetDescendants() do
+		if v.Name == "GumdropHitbox" and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Bean") then
+			game:GetService("Players").LocalPlayer.Character.Bean.CFrame = CFrame.new(v.Position)
+			task.wait(0.1)
+			game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("InteractObject"):FireServer(v)
+		end
+	end
 end
