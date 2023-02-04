89e83b076d2ded744524b7b84d299ce9c69b4405
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-04 17:47:33 +0300

    Update ChaoticBeanSimulatorAutofarm.lua

--- ChaoticBeanSimulatorAutofarm.lua
+++ ChaoticBeanSimulatorAutofarm.lua
@@ -1,4 +1,50 @@
 -- we love SkyTheFloof
+repeat task.wait() until game:IsLoaded()
+local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
+local httpservice = game:GetService('HttpService')
+
+function shop()
+	local gameId
+	gameId = game.PlaceId
+	local servers = {}
+	local req = httprequest({
+		Url = "https://games.roblox.com/v1/games/" .. gameId .. "/servers/Public?sortOrder=Desc&limit=100"
+	})
+	local body = httpservice:JSONDecode(req.Body)
+	if body and body.data then
+		for i, v in next, body.data do
+			if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing > 4 then
+				table.insert(servers, 1, v.id)
+			end
+		end
+	end
+	task.spawn(function()
+		while task.wait(0.5) do
+				if #servers > 0 then
+					game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, servers[math.random(1, #servers)], Players.LocalPlayer)
+				end
+		end
+	end)
+	game:GetService("TeleportService").TeleportInitFailed:Connect(function()
+		game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, servers[math.random(1, #servers)], Players.LocalPlayer)
+	end)    
+end
+
+for i,v in next, game:GetService('Players'):GetPlayers() do
+    if v.UserId == 9779414 then -- SkyTheFloof
+        shop()
+    end
+end
+
+game:GetService('Players').PlayerAdded:Connect(function(v)
+    if v.UserId == 9779414 then -- SkyTheFloof
+        shop()
+    end
+end)
+
+queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/random/main/ChaoticBeanSimulatorAutofarm.lua'))()")
+
 task.spawn(
     function()
         while task.wait() do
