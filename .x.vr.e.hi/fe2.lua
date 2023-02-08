6aa6429c0fba7e31e2813a7d91cf15843f8c735b
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-08 15:19:24 +0300

    a

--- .x.vr.e.hi/fe2.lua
+++ .x.vr.e.hi/fe2.lua
@@ -64,7 +64,7 @@ end
 function Time(targetpos)
    --local tme = (targetpos - game.Players.LocalPlayer.Character:WaitForChild('HumanoidRootPart').Position).Magnitude / game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid").WalkSpeed
 	local tme = (game.Players.LocalPlayer.Character.Humanoid.RootPart.Position - targetpos).Magnitude / 20
-	return tme - 0.38
+	return tme - 0.15
 end
 
 workspace.Multiplayer.DescendantAdded:Connect(function(t)
