436878adb51cbb2c46b9d015a0ef0bdeced34353
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-23 22:08:35 +0300

    Update DefendTheTrainFucker.lua

--- DefendTheTrainFucker.lua
+++ DefendTheTrainFucker.lua
@@ -5,6 +5,7 @@ local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or 
 local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 local httpservice = game:GetService('HttpService')
 local Players = players
+game:GetService('RunService'):Stop() -- optimization
 
 function shop()
 	local gameId
